034fb7fe37ee588b4e3cf648e7637d61acbee84f
unknown <aconfino@gmail.com>
2014-01-16 17:26:57 -0500

    added test

--- src/test/java/com/confino/demo/BasicTest.java
+++ src/test/java/com/confino/demo/BasicTest.java
@@ -3,24 +3,26 @@ package com.confino.demo;
 import org.junit.Test;
 
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.response.ResponseBody;
 
 import static com.jayway.restassured.RestAssured.*;
 import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+@SuppressWarnings("rawtypes")
 public class BasicTest {
 	
 	private String url = "http://api.openweathermap.org/data/2.1/find/city?lat=40.2605&lon=-75.6155&cnt=3";
-	
+
 	@Test
-	public void simpleTest(){
-		assertTrue(get(url).asString().contains("Pottstown"));
-	}
-	
-	@Test
-	public void demo2Test(){
-		assertTrue(get(url).body().jsonPath().getString("list.name").contains("Sanatoga"));
+	public void demoTest(){
+		ResponseBody body = get(url).body();
+		assertTrue(body.jsonPath().getString("list.id[0]").equals("5210764"));
+		assertTrue(body.jsonPath().getString("list.distance[0]").equals("2.68"));
+		assertTrue(body.jsonPath().getString("list.coord[0].lon").equals("-75.59518"));
+		assertTrue(body.jsonPath().getString("list.coord[0].lat").equals("40.2451"));
+		assertTrue(body.jsonPath().getString("list.name[0]").equals("Sanatoga"));
 	}
 
 }
