0aa2f13a8382a9ac2491cbf8dc7cf7fb948a0876
aconfino <aconfino@gmail.com>
2014-01-17 08:32:52 -0500

    Update BasicTest.java

--- src/test/java/com/confino/demo/BasicTest.java
+++ src/test/java/com/confino/demo/BasicTest.java
@@ -16,36 +16,37 @@ public class BasicTest {
 	
 	@Test
 	public void simpleGetTest(){
-		given().
-        	param("lat", "40.2605").
-        	param("lon", "-75.6155").
-        	param("cnt", "3").
-        when().
-        	get("http://api.openweathermap.org/data/2.1/find/city").
-        then().
-        	body(containsString("Sanatoga"));
+	  given().
+            param("lat", "40.2605").
+            param("lon", "-75.6155").
+            param("cnt", "3").
+          when().
+            get("http://api.openweathermap.org/data/2.1/find/city").
+          then().
+            body(containsString("Sanatoga"));
 	}
 	
 	@Test
 	public void simplePostTest(){
-		given().
-        	queryParam("lat", "40.2605").
-        	queryParam("lon", "-75.6155").
-        	queryParam("cnt", "3").
-        when().
-        	post("http://api.openweathermap.org/data/2.1/find/city").
-        then().
-        	body(containsString("Sanatoga"));
+	   given().
+             queryParam("lat", "40.2605").
+             queryParam("lon", "-75.6155").
+             queryParam("cnt", "3").
+           when().
+             post("http://api.openweathermap.org/data/2.1/find/city").
+           then().
+             body(containsString("Sanatoga"));
 	}
 	
 	@Test
 	public void basicTest(){
-		ResponseBody body = get(url).body();
-		assertTrue(body.jsonPath().getString("list.id[0]").equals("5210764"));
-		assertTrue(body.jsonPath().getString("list.distance[0]").equals("2.68"));
-		assertTrue(body.jsonPath().getString("list.coord[0].lon").equals("-75.59518"));
-		assertTrue(body.jsonPath().getString("list.coord[0].lat").equals("40.2451"));
-		assertTrue(body.jsonPath().getString("list.name[0]").equals("Sanatoga"));
+	  ResponseBody body = get(url).body();
+	  
+          assertTrue(body.jsonPath().getString("list.id[0]").equals("5210764"));
+	  assertTrue(body.jsonPath().getString("list.distance[0]").equals("2.68"));
+          assertTrue(body.jsonPath().getString("list.coord[0].lon").equals("-75.59518"));
+	  assertTrue(body.jsonPath().getString("list.coord[0].lat").equals("40.2451"));
+          assertTrue(body.jsonPath().getString("list.name[0]").equals("Sanatoga"));
 	}
 
 }
