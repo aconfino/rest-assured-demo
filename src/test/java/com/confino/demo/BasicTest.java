7319dbad0b2338b8118ff64dd5e3207786f72191
unknown <aconfino@gmail.com>
2014-01-16 19:54:08 -0500

    added tests

--- src/test/java/com/confino/demo/BasicTest.java
+++ src/test/java/com/confino/demo/BasicTest.java
@@ -1,22 +1,45 @@
 package com.confino.demo;
 
+import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-import com.jayway.restassured.response.Response;
 import com.jayway.restassured.response.ResponseBody;
-
-import static com.jayway.restassured.RestAssured.*;
-import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
 
 @SuppressWarnings("rawtypes")
 public class BasicTest {
 	
 	private String url = "http://api.openweathermap.org/data/2.1/find/city?lat=40.2605&lon=-75.6155&cnt=3";
-
+	
 	@Test
-	public void demoTest(){
+	public void simpleGetTest(){
+		given().
+        	param("lat", "40.2605").
+        	param("lon", "-75.6155").
+        	param("cnt", "3").
+        when().
+        	get("http://api.openweathermap.org/data/2.1/find/city").
+        then().
+        	body(containsString("Sanatoga"));
+	}
+	
+	@Test
+	public void simplePostTest(){
+		given().
+        	queryParam("lat", "40.2605").
+        	queryParam("lon", "-75.6155").
+        	queryParam("cnt", "3").
+        when().
+        	post("http://api.openweathermap.org/data/2.1/find/city").
+        then().
+        	body(containsString("Sanatoga"));
+	}
+	
+	@Test
+	public void basicTest(){
 		ResponseBody body = get(url).body();
 		assertTrue(body.jsonPath().getString("list.id[0]").equals("5210764"));
 		assertTrue(body.jsonPath().getString("list.distance[0]").equals("2.68"));
